742d5664cd4ef2597f67ac9dd464d979ffc19ece
nyg0o0 <yaya56790@gmail.com>
2016-04-02 15:17:23 +0800

    add lines

--- BeanGame.java
+++ BeanGame.java
@@ -8,15 +8,13 @@ import javafx.scene.shape.*;
 import java.lang.Math;
 
 public class BeanGame extends Application {
-	public void start(Stage primaryStage) {   
-		Pane pane = new Pane();
-		Polyline polyline = new Polyline();
-	
-
-		
+	public void start(Stage primaryStage) { 
 		final double WIDTH = 400, HEIGHT = 400;
-		double centerX = WIDTH / 2, centerY = HEIGHT /2;
-		//double widthUnit = WIDTH / 10, heightUnit = WIDTH / 10 ;
+		double centerX = WIDTH / 2, centerY = HEIGHT /2;	
+		
+		Pane pane = new Pane();
+		
+		Polyline polyline = new Polyline();
 		polyline.getPoints().addAll(new Double[]{	centerX- 0.05* WIDTH , centerY - (Math.sqrt(3)/2)* 0.3* HEIGHT - 0.1* HEIGHT,
 													centerX- 0.05* WIDTH , centerY - (Math.sqrt(3)/2)* 0.3* HEIGHT,
 													centerX- 0.35* WIDTH , centerY + (Math.sqrt(3)/2)* 0.3* HEIGHT,
@@ -28,6 +26,15 @@ public class BeanGame extends Application {
 													centerX+ 0.05* WIDTH , centerY - (Math.sqrt(3)/2)* 0.3* HEIGHT - 0.1* HEIGHT
 													});
 		pane.getChildren().add(polyline);
+		
+		Line[] line = new Line[7];
+		for(int i=0;i<7;i++){
+			line[i] = new Line(	centerX+ 0.0875* WIDTH*(i-3),centerY + (Math.sqrt(3)/2)* 0.3* HEIGHT + 0.15* HEIGHT - 0.15* HEIGHT,
+								centerX+ 0.0875* WIDTH*(i-3),centerY + (Math.sqrt(3)/2)* 0.3* HEIGHT + 0.15* HEIGHT);
+			pane.getChildren().add(line[i]);
+		}
+		
+
 		
 		Scene scene = new Scene(pane,WIDTH,HEIGHT);
 		
